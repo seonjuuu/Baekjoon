a2fa1a13d790447f2a55a18bd71f9af1f98e9fa0
seonjuuu <mhnaa6469@gmail.com>
2025-02-14 00:26:21 +0900

    4번

--- Step4.py
+++ Step4.py
@@ -18,3 +18,12 @@ for i in A:
 N = int(input())
 A = list(map(int,input().split()))
 print(min(A),max(A))
+
+
+#4
+A = []
+for i in range(9):
+    a = int(input())
+    A.append(a)
+print(max(A))
+print(A.index(max(A))+1)
