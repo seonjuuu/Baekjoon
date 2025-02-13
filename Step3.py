01b6158b19ab2cbddd9cf3594ffad0fa52d63bcb
seonjuuu <mhnaa6469@gmail.com>
2025-02-13 20:32:16 +0900

    3번

--- Step3.py
+++ Step3.py
@@ -1,4 +1,3 @@
-
 #1 
 N = int(input())
 for i in range(1,10):
@@ -10,3 +9,9 @@ for i in range(T):
     A,B = map(int,input().split())
     print(A+B)
 
+#3
+n = int(input())
+sum = 0                 #sum 변수 선언
+for i in range(1,n+1):
+    sum += i
+print(sum)
