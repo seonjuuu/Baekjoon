a4f78650caafc0fb267a0a3905cc0af060cc8430
seonjuuu <mhnaa6469@gmail.com>
2025-02-15 21:29:05 +0900

    4번

--- Step5.py
+++ Step5.py
@@ -14,3 +14,12 @@ T = int(input())
 for i in range(T):
     S = input()
     print(S[0]+S[-1])
+    
+    
+#4
+# ord(문자) : 아스키 코드를 반환
+# chr(숫자) : 숫자에 맞는 아스키 코드를 반환 (숫자->문자)
+print(ord('A'))
+print(ord('9'))  # 9를 숫자가 아닌 문자로 받아서, 그의 아스키 코드를 반환
+S = input()
+print(ord(S))
