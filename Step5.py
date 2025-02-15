c8b1b68994d2b13b6cf4de352aa6ce3591b9e282
seonjuuu <mhnaa6469@gmail.com>
2025-02-15 21:33:11 +0900

    6번

--- Step5.py
+++ Step5.py
@@ -42,3 +42,12 @@ for i in C:
         print(S.index(i), end=' ')
     else:
         print(-1,end=' ')
+        
+#6
+##find함수 이용
+# find 함수는 어떤 찾는 문자가 문자열 안에서 첫 번째에 위치한 순서를 숫자로 출력한다.
+#만일 찾는 문자가 문자열 안에 없는 경우에는 -1을 출력하는 함수이다.
+S = input()
+C = 'abcdefghijklmnopqrstuvwxyz'
+for i in C:
+    print(S.find(i), end=' ')
