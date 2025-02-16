91a6aca427b0fb816b2a58b53981684f38fba5ec
seonjuuu <mhnaa6469@gmail.com>
2025-02-16 13:44:40 +0900

    3번

--- Step6.py
+++ Step6.py
@@ -12,3 +12,20 @@ chess = [1,1,2,2,2,8]
 A = list(map(int,input().split()))
 for i in range(len(A)):
     print(chess[i]-A[i], end=" ")
+    
+    
+#3 -> 뒤 공백은 생각X !! 
+# 별이 증가하는 부분과 줄어드는 부분 나눠서
+# range(, ,-1) : 역순으로 출력
+# 별은 2N-1개가 찍힘 (N=줄)
+
+# N입력받기
+N = int(input())
+
+# 별의 개수가 증가하는 부분
+for i in range(1,N+1):
+    print(" " * (N-i) + "*" * (2*i-1))
+    
+# 별의 개수가 감소하는 부분
+for i in range(N-1,-0,-1):
+    print(" " * (N-i) + "*" * (2*i-1))
