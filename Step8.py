1a82648bd3981d92553b9cf7b976ce7b6620191c
seonjuuu <mhnaa6469@gmail.com>
2025-02-19 20:23:47 +0900

    2번

--- Step8.py
+++ Step8.py
@@ -10,3 +10,14 @@ for i in range(len(N)):
     result += S.index(N[i])*(B**i)
     
 print(result)
+
+
+#2 -> 몫이 0일때까지 -> 나머지들만 진법변환
+S = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+N, B = map(int,input().split())
+result = ''
+while N:                   #N이 0일 될때까지
+    result = S[N%B] + result
+    N=N//B                 #몫
+    
+print(result)    
