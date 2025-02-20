6a7fca1fa8625ff493582f66f40b5d715a26e50d
seonjuuu <mhnaa6469@gmail.com>
2025-02-20 15:46:36 +0900

    3번

--- Step9.py
+++ Step9.py
@@ -22,3 +22,19 @@ if (len(A) >= k):
     print(A[k-1])
 else:
     print("0")
+    
+    
+#3 -> join / 리스트 합을 sum함수로 구할수있음 / sep="" : 출력
+while(1):
+    n = int(input())
+    if (n==-1):
+        break
+    A = []
+    for i in range(1,n):
+        if(n%i==0):
+            A.append(i)
+
+    if sum(A) == n :
+        print(n,' = '," + ".join(str(i) for i in A), sep="")
+    else:
+        print(n,"is NOT perfect.")
