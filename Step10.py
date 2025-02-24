44783a391f4a9636000d838db04765b137365cd6
seonjuuu <mhnaa6469@gmail.com>
2025-02-24 15:48:53 +0900

    7번

--- Step10.py
+++ Step10.py
@@ -59,14 +59,15 @@ else:
     print("Error")
     
 #7
-a, b, c = map(int, input().split())
-if a == b == c == 0 :
-    break
-if sum((a, b, c)) - max((a, b, c)) <= max((a, b, c)) :
-    print("Invalid")
-elif a == b == c :
-    print('Equilateral')
-elif a == b or b == c or a == c :
-    print("Isosceles")
-else :
-    print("Scalene")
+while(1):
+    a, b, c = map(int, input().split())
+    if a == b == c == 0 :
+        break
+    if sum((a, b, c)) - max((a, b, c)) <= max((a, b, c)) :
+        print("Invalid")
+    elif a == b == c :
+        print('Equilateral')
+    elif a == b or b == c or a == c :
+        print("Isosceles")
+    else :
+        print("Scalene")
