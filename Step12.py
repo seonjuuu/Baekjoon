0f4cdc333e6272a1c2e8f312d475a4997d15ffc2
seonjuuu <mhnaa6469@gmail.com>
2025-03-01 15:33:42 +0900

    1번

--- Step12.py
+++ Step12.py
@@ -1,5 +1,10 @@
 #1 -> 완전 이진 탐색
 #경우1 -> for 삼중문
+N, M = map(int,input().split())
+list = list(map(int,input().split()))
+result = []
 for i in range(N):
     for j in range(i+1,N):
         for k in range(j+1,N):
+            if (list[i]+list[j]+list[k] <= M):
+                result.append(list[i]+list[j]+list[k])
