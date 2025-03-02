d63041acc398e0a1f9fb4c915784ae7b725c3950
seonjuuu <mhnaa6469@gmail.com>
2025-03-02 21:51:11 +0900

    2번

--- Step12.py
+++ Step12.py
@@ -12,3 +12,9 @@ for i in range(N):
                 continue
 
 print(max(result))
+
+
+#2
+#1) N보다 작은 수들 다 확인하는 과정 -> for문
+#2) i의 각 자릿수를 더함
+# (map(int,str(i))) -> 정수인 i를 문자열로 변환(str(i))=>변환하게 되면 문자열의 각 문자로 쪼개진다!! -> map함수를 이용하여 각 문자에 int함수를 적용하여 정수로 변환
