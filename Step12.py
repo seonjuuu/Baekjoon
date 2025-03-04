4e10b1e08e3ef69e1053e8ccd2e5f7a56d676340
seonjuuu <mhnaa6469@gmail.com>
2025-03-04 20:30:24 +0900

    4번

--- Step12.py
+++ Step12.py
@@ -65,3 +65,5 @@ for a in range(n-7):   #체스판
                     else : 
                         w_start += 1
                 else:
+                    if bored[i][j] == "W":
+                        w_start += 1 
