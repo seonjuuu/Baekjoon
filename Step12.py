7de5230df20cc95954bde6e3e3a4da2cf9ee87dc
seonjuuu <mhnaa6469@gmail.com>
2025-03-06 22:52:02 +0900

    5번

--- Step12.py
+++ Step12.py
@@ -86,3 +86,8 @@ num =  666   # 666이 첫번째 종말의 수이므로, 666부터 시작 !
 
 if '666' in str(num): #num이 종말의 수일때
         cnt += 1
+        
+        
+if cnt == n :  #n번째 종말의 수를 찾았다면
+        print(num)
+        break
