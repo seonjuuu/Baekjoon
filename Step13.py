c7d6a82cf0bb4ba7e0713d7f7b41170e53c893a3
seonjuuu <mhnaa6469@gmail.com>
2025-03-10 19:47:09 +0900

    1번

---
+++ Step13.py
@@ -0,0 +1,142 @@
+# import sys
+# input = sys.stdin.readline 을 적어줘야 빠름
+
+#1
+list = []
+N = int(input())
+for i in range(N):
+    a = int(input())
+    list.append(a)
+
+list.sort()
+for i in range(N):
+    print(list[i])    
+
+#2
+list = []
+for i in range(5):
+    a = int(input())
+    list.append(a)
+
+list.sort()
+print(sum(list)//5)
+print(list[2])
+
+#3
+N, k = map(int,input().split())
+x = list(map(int,input().split()))
+x.sort(reverse=True)   # 내림차순
+print(x[k-1])
+
+#4
+#시간초과
+#import sys
+#input = sys.stdin.readline()
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+list = []
+for i in range(N):
+    k = int(input())
+    list.append(k)
+list.sort()
+for i in list:
+    print(i)
+
+#6
+N = list(map(int,str(input)))
+N.sort(reverse=True)  # sorT=정렬 / [::-1] = 자리옮기기(정렬x)
+for i in range(len(N)):
+    print(N[i],end="")
+
+
+#7 -> sort 함수 기준 정하기
+#이차원 리스트 -> [[x1,y1],[x2,y2] . . .] 
+#list.sort() = x을 기준으로 정렬 -> y을 기준으로 정렬
+N = int(input())
+list = []
+
+for i in range(N):
+    x,y = map(int,input().split())
+    list.append([x,y])
+
+list.sort()  
+for i in range(N):             
+    print(list[i][0], list[i][1])
+# for i in list: 
+# print(i[0], i[1]) 
+
+#8
+#두번째 값을 기준 -> sort 함수의 기준을 바꿔야함
+#list.sort(key = lambda x: (x[1], x[0]))
+#경우1
+N = int(input())
+list = []
+for i in range(N):
+    x,y = map(int,input().split())
+    list.append([y,x])  # y,x 순서바꿔서 append
+ 
+list.sort()  # 순서바꿨기에, y가 x보다 먼저 -> y기준으로 정렬 후 x로 정렬
+for i in range(N):
+    print(list[i][1],list[i][0])
+
+##경우2
+N = int(input())
+list = []
+for i in range(N):
+    a,b = map(int,input().split())
+    list.append([a,b])  
+ 
+list.sort(key = lambda x: (x[1],x[0])) #x1 = y좌표를 기준으로 정렬 후 x0 = x좌표로 정렬
+for i in range(N):
+    print(list[i][0],list[i][1])
+
+
+#9
+#기본 list.sort() -> 사전순으로 정렬(sort 정렬이 문자까지 정렬)
+#정렬 우선순위의 역순
+#
+#정렬 순서를 주의해야 되는데, 상위 조건 A와 하위 조건 B가 있으면 먼저 B로 정렬을 한 후에 A로 정렬을 해야 원하는 결과를 얻을 수 있다. -> 최종적으로 원하는 출력의 정렬은, A의 조건이므로, A를 마지막에!
+N = int(input())
+list = []
+for i in range(N):
+    s = input()
+    if s not in list:
+        list.append(s)
+list.sort(key=lambda x : (len(x),x)) # 1. len(x) -> 길이로 정렬 2. x -> 사전 순
+# 챗
+#list.sort(key=len) -> 길이순
+#list.sort() # -> 사전순
+for i in list:
+    print(i)
+
+
+#10
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+list = []
+for i in range(N):
+    age, name = input().split()
+    age = int(age)
+    list.append([age,name])
+
+list.sort(key=lambda x : x[0]) #기본적으로 입력순 정렬 -> 나이(x[0]로 정렬
+for i in list:
+    print(i[0],i[1])
+
+#11
+#list.index(i) -> 시간초과
+#딕셔너리 이용
+N = int(input())
+X = list(map(int,input().split()))
+arr = (set(X))    # 중복제거
+arr = sorted(arr)                  # [-10,-9,2,4]
+dic = {arr[i]:i for i in range(len(arr))} # {-10:0, -9:1, 2:2, 4:3} #arr(key)에 인덱스 값을 넣어줌
+for i in X:
+    print(dic[i],end=" ") #딕셔너리를 통해 값을 찾아줌
+
+
+
