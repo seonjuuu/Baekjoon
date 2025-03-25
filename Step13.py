2b03f0fb4d7376252bf97d58d288156887750ed9
seonjuuu <mhnaa6469@gmail.com>
2025-03-25 19:20:34 +0900

    11번

--- Step13.py
+++ Step13.py
@@ -122,4 +122,4 @@ N = int(input())
 X = list(map(int,input().split()))
 arr = (set(X))      # 중복제거
 arr = sorted(arr)   # [-10,-9,2,4]
-dic = {arr[i]:i for i in range(len(arr))}
+dic = {arr[i]:i for i in range(len(arr))}  # {-10:0, -9:1, 2:2, 4:3} #arr(key)에 인덱스 값을 넣어줌
