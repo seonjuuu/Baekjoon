dacd2fcdcf959c9dbf986090355268ab2b3d2114
seonjuuu <mhnaa6469@gmail.com>
2025-04-10 22:09:41 +0900

    4번

--- 200.py
+++ 200.py
@@ -86,3 +86,4 @@ for i in range(n):
         answer.append("-")
     else:                 # stack의 TOP이 입력한 수가 아니면 주어진 스택을 만들 수 없다.
         print("NO")       # 왜냐하면 TOP이 num보다 크면 num은 TOP보다 더 아래에 쌓여있기 때문이다.
+        flag = 1
