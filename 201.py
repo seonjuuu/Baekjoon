8d356b44e82b1a6446172c7fe3b3f08f79a248d1
seonjuuu <mhnaa6469@gmail.com>
2025-05-06 20:21:20 +0900

    2번

--- 201.py
+++ 201.py
@@ -50,4 +50,4 @@ for i in range(len(S)):
             stack.pop()
             cnt = cnt+len(stack)  # 각 레이더가 영향을 주는 막대 개수
         else:
-            if S[i-1] == "(":
+            if S[i-1] == "(":  #레이더인 경우
